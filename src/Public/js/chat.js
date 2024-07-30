4d658170869534549ecb2bb3049e83ff1c714225
AdriánFernández <adrianfer_87@hotmail.com>
2024-07-30 10:05:02 -0300

    Practica-de-integracion-sobre-tu-ecommerce-1.1

--- src/Public/js/chat.js
+++ src/Public/js/chat.js
@@ -1,5 +1,3 @@
-import { logger } from "../../utils/logger.js";
-
 // Inicializar el socket.io
 const socket = io();
 let user;
@@ -7,7 +5,7 @@ let user;
 // Verificar si el usuario ya está almacenado en localStorage
 if (localStorage.getItem('user')) {
     user = localStorage.getItem('user');
-    logger.info(`Usuario recuperado - Log de src/Public/js/chat.js: ${user}`);
+    console.log(`Usuario recuperado - Log de src/Public/js/chat.js: ${user}`);
 } else {
     // Mostrar un cuadro de diálogo para que el usuario ingrese su nombre
     Swal.fire({
@@ -21,7 +19,7 @@ if (localStorage.getItem('user')) {
         allowOutsideClick: false
     }).then(result => {
         user = result.value;
-        logger.info(`Usuario ingresado - Log de src/Public/js/chat.js: ${user}`);
+        console.log(`Usuario ingresado - Log de src/Public/js/chat.js: ${user}`);
         localStorage.setItem('user', user); // Guardar el usuario en localStorage
     });
 }
@@ -52,15 +50,15 @@ function sendMessage() {
         })
         .then(response => {
             if (response.ok) {
-                logger.info('Mensaje enviado al servidor - Log de src/Public/js/chat.js');
+                console.log('Mensaje enviado al servidor - Log de src/Public/js/chat.js');
                 // Agregar el mensaje al contenedor de mensajes del chat
                 // appendMessageToUI(`${user}: ${message}`); // Duplica mensaje emisor, por eso se comenta
             } else {
-                logger.error('Error al enviar el mensaje al servidor - Log de src/Public/js/chat.js');
+                console.log('Error al enviar el mensaje al servidor - Log de src/Public/js/chat.js');
             }
         })
         .catch(error => {
-            logger.error('Error al enviar el mensaje - Log de src/Public/js/chat.js:', error);
+            console.log('Error al enviar el mensaje - Log de src/Public/js/chat.js:', error);
         });
 
         // Enviar el mensaje al servidor para transmitirlo en tiempo real a todos los clientes
@@ -80,7 +78,7 @@ function appendMessageToUI(message) {
 // Manejar los mensajes recibidos del servidor y mostrarlos en el cliente
 socket.on('message', ({ user, message }) => {
     appendMessageToUI(`${user}: ${message}`);
-    logger.info(`Mensaje recibido de ${user} - Log de src/Public/js/chat.js: ${message}`);
+    console.log(`Mensaje recibido de ${user} - Log de src/Public/js/chat.js: ${message}`);
 });
 
 // Enviar mensaje cuando se hace clic en el botón de enviar
