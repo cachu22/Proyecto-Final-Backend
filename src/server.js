b187150630927abd128be2aa30d420af3abb6bac
AdriánFernández <adrianfer_87@hotmail.com>
2024-09-15 22:45:40 -0300

    Protecto-Final-Backend-2.4

--- src/server.js
+++ src/server.js
@@ -102,10 +102,7 @@ dotenv.config({ path: envFilePath });
 app.use(cors());
 app.use(addLogger);
 
-const connection = mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+const connection = mongoose.connect(process.env.MONGO_URL)
     .then(() => console.log('Conexión a la base de datos exitosa'))
     .catch((err) => console.error('Error al conectar a la base de datos:', err));
 
